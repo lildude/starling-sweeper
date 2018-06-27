3bd88f611845b3e99ed56fd58d40dcbd45aad953
Colin Seymour <lildood@gmail.com>
2018-06-27 13:53:08 +0100

    Add validateSignature tests

--- main_test.go
+++ main_test.go
@@ -32,3 +32,32 @@ func TestRoundUp(t *testing.T) {
 		})
 	}
 }
+
+func TestValidateSignature(t *testing.T) {
+	//t.Parallel()
+	// Discard logging info
+	log.SetOutput(ioutil.Discard)
+
+	body := []byte(`{"test":"body"}`)
+	signature := "C3zcs4qlrazPXGdPacksD/RhFeqBIjm/YkOjvZPo28OxJaUgaZT3RoTuJyGmlJkDWz/viPyWJvTJLbRz2tE7ww=="
+	testCases := []struct {
+		name string
+		body []byte
+		sig  string
+		res  bool
+	}{
+		{"valid signature and body", body, signature, true},
+		{"invalid signature", body, "foobar", false},
+		{"invalid body", []byte(`{"foo":"bar"}`), signature, false},
+	}
+
+	for _, tc := range testCases {
+		tc := tc
+		t.Run(tc.name, func(t *testing.T) {
+			//t.Parallel()
+			if validateSignature(tc.body, tc.sig) != tc.res {
+				t.Errorf("%v failed", tc.name)
+			}
+		})
+	}
+}
