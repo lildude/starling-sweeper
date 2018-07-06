a97d744dce4d32cae9af160451b08d3e2aba2988
Colin Seymour <lildood@gmail.com>
2018-07-06 18:19:53 +0100

    Refactor for testing and in prep for sweeper functionality

--- main.go
+++ main.go
@@ -18,10 +18,11 @@ import (
 )
 
 type Specification struct {
-	Port                string `required:"true" envconfig:"PORT"`
-	WebhookSecret       string `required:"true" split_words:"true"`
-	SavingGoal          string `required:"true" split_words:"true"`
-	PersonalAccessToken string `required:"true" split_words:"true"`
+	Port                string  `required:"true" envconfig:"PORT"`
+	WebhookSecret       string  `required:"true" split_words:"true"`
+	SavingGoal          string  `required:"true" split_words:"true"`
+	PersonalAccessToken string  `required:"true" split_words:"true"`
+	SweepThreshold      float64 `split_words:"true"`
 }
 
 var s Specification
@@ -37,6 +38,7 @@ func main() {
 	http.ListenAndServe(":"+s.Port, nil)
 }
 
+// TODO: Add sweeper support - when a payment comes in above a set threshold, and we have a balance above 0, move the balance to a pot.
 func TxnHandler(w http.ResponseWriter, r *http.Request) {
 	// Return OK as soon as we've received the payload - the webhook doesn't care what we do with the payload so no point holding things back.
 	w.WriteHeader(http.StatusOK)
@@ -62,48 +64,57 @@ func TxnHandler(w http.ResponseWriter, r *http.Request) {
 	log.Println("INFO: type:", wh.Content.Type)
 	log.Println("INFO: amount:", wh.Content.Amount)
 
-	// Don't round-up anything other than card transactions
-	if wh.Content.Type != "TRANSACTION_CARD" && wh.Content.Type != "TRANSACTION_MOBILE_WALLET" {
-		log.Println("INFO: ignoring non-card transaction")
-		w.WriteHeader(http.StatusOK)
+	// Ignore anything other than card transactions or transactions above the threshold if a threshold is set
+	if (wh.Content.Type != "TRANSACTION_CARD" && wh.Content.Type != "TRANSACTION_MOBILE_WALLET") || (wh.Content.Amount > s.SweepThreshold) {
+		log.Printf("INFO: ignoring %s transaction\n", wh.Content.Type)
 		return
 	}
 
-	// Don't round-up incoming (i.e. positive) amounts
-	if wh.Content.Amount >= 0.0 {
-		log.Println("INFO: ignoring inbound transaction")
-		w.WriteHeader(http.StatusOK)
-		return
+	var ra int64
+
+	if wh.Content.Type == "TRANSACTION_CARD" || wh.Content.Type == "TRANSACTION_MOBILE_WALLET" {
+		if wh.Content.Amount >= 0.0 {
+			log.Println("INFO: ignoring inbound card transaction")
+			return
+		}
+		// Round up to the nearest major unit
+		amtMinor := math.Round(wh.Content.Amount * -100)
+		ra = roundUp(int64(amtMinor))
+		log.Println("INFO: round-up yields:", ra)
+
+		// Don't try and transfer a zero value to the savings goal
+		if ra == 0 {
+			log.Println("INFO: nothing to round-up")
+			return
+		}
 	}
 
-	// Round up to the nearest major unit
-	amtMinor := math.Round(wh.Content.Amount * -100)
-	ra := roundUp(int64(amtMinor))
-	log.Println("INFO: round-up yields:", ra)
-
-	// Don't try and transfer a zero value to the savings goal
-	if ra == 0 {
-		log.Println("INFO: nothing to round-up")
-		w.WriteHeader(http.StatusOK)
-		return
+	if wh.Content.Amount > s.SweepThreshold {
+		ra = getBalance(wh.Content.TransactionUID)
 	}
 
 	// Transfer the funds to the savings goal
-	ctx := context.Background()
-	sb := newClient(ctx, s.PersonalAccessToken)
-	amt := starling.Amount{
-		MinorUnits: ra,
-		Currency:   wh.Content.SourceCurrency,
+	// TODO: Go noob goal: Learn how to stub the requests to the API to return OK
+	//       Until then, we skip this section entirely if the access token env var isn't set,
+	//       which will be the case during testing. We wouldn't get this far outside of testing.
+	if s.PersonalAccessToken != "" {
+		ctx := context.Background()
+		sb := newClient(ctx, s.PersonalAccessToken)
+		amt := starling.Amount{
+			MinorUnits: ra,
+			Currency:   wh.Content.SourceCurrency,
+		}
+
+		txn, resp, err := sb.AddMoney(ctx, s.SavingGoal, amt)
+		if err != nil {
+			log.Println("ERROR: failed to move money to savings goal:", err)
+			log.Println("ERROR: Starling Bank API returned:", resp.Status)
+			return
+		}
+		log.Println("INFO: Txn ID ", txn)
 	}
 
-	txn, resp, err := sb.AddMoney(ctx, s.SavingGoal, amt)
-	if err != nil {
-		log.Println("ERROR: failed to move money to savings goal:", err)
-		log.Println("ERROR: Starling Bank API returned:", resp.Status)
-		return
-	}
-
-	log.Println("INFO: round-up successful:", txn)
+	log.Println("INFO: round-up successful")
 	return
 }
 
@@ -143,4 +154,12 @@ func roundUp(txn int64) int64 {
 	// to the next 100.
 	amtRound := (txn + 99) / 100 * 100
 	return amtRound - txn
+}
+
+// Grabs txn deets and removes txn amt from balance and returns the minor units
+func getBalance(txnUid string) int64 {
+	return 0
+	//ctx := context.Background()
+	//sb := newClient(ctx, s.PersonalAccessToken)
+
 }
